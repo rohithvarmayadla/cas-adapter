4172183fff91a536b35aaee0bcd133f08ac77fad
Jebb Burditt <jebb.burditt@gmail.com>
2025-04-08 13:39:37 -0700

    add logging

---
+++ CASInterfaceService/Hosting/ObservabilityExtensions.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Formatting.Compact;
+using System;
+using System.Net.Http;
+using System.Reflection;
+using System.Security.Claims;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+public static class ObservabilityExtensions
+{
+    private const string logOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3} {SourceContext}] {Message:lj}{NewLine}{Exception}";
+
+    /// <summary>
+    /// Configures observability instruments like logging to the web application and return an initial logger
+    /// </summary>
+    /// <returns>A logger that can be used during starting up the web application</returns>
+    public static ILogger ConfigureWebApplicationObservability(this WebApplicationBuilder builder)
+    {
+        Serilog.Debugging.SelfLog.Enable(Console.Error);
+        var logger = CreateBootstrapLogger(builder.Configuration);
+
+        var serviceName = Assembly.GetEntryAssembly()!.GetName().Name ?? throw new InvalidOperationException("Could not establish the service name");
+
+        //builder.Host.UseSerilog((context, services, configuration) => configuration
+        //            .ReadFrom.Configuration(context.Configuration)
+        //            .ReadFrom.Services(services), preserveStaticLogger: true);
+        builder.Services.AddSerilog((services, config) =>
+        {
+            config
+              .ReadFrom.Configuration(builder.Configuration)
+              .ReadFrom.Services(services)
+              .Enrich.WithProperty("service", serviceName)
+              .WriteTo.Console(outputTemplate: logOutputTemplate);
+        });
+
+        //if (hostingContext.HostingEnvironment.IsDevelopment())
+        //{
+        //    logger.WriteTo.Console();
+        //}
+        //else
+        //{
+        //    loggerConfiguration.WriteTo.Console(formatter: new RenderedCompactJsonFormatter());
+        //    var splunkUrl = hostingContext.Configuration.GetSplunkUrl();
+        //    var splunkToken = hostingContext.Configuration.GetSplunkToken();
+        //    if (string.IsNullOrWhiteSpace(splunkToken) || string.IsNullOrWhiteSpace(splunkUrl))
+        //    {
+        //        Log.Error($"Splunk logging sink is not configured properly, check SPLUNK_TOKEN and SPLUNK_URL env vars");
+        //    }
+        //    else
+        //    {
+        //        loggerConfiguration
+        //            .WriteTo.EventCollector(
+        //                splunkHost: splunkUrl,
+        //                eventCollectorToken: splunkToken,
+        //                messageHandler: new HttpClientHandler
+        //                {
+        //                    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+        //                },
+        //                renderTemplate: false);
+        //    }
+        //}
+
+        return logger;
+    }
+
+    /// <summary>
+    /// Adds observability instruments like logging to the web application's middleware pipelines
+    /// </summary>
+    public static void UseObservabilityMiddleware(this WebApplication webApplication)
+    {
+        webApplication.UseSerilogRequestLogging(opts =>
+        {
+            opts.IncludeQueryInRequestPath = true;
+            opts.EnrichDiagnosticContext = (diagCtx, httpCtx) =>
+            {
+                diagCtx.Set("User", httpCtx.User.FindFirst(ClaimTypes.Name)?.Value ?? string.Empty);
+            };
+        });
+    }
+
+    private static ILogger CreateBootstrapLogger(IConfiguration configuration)
+    {
+        Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).WriteTo.Console(outputTemplate: logOutputTemplate).CreateBootstrapLogger();
+        return (ILogger)Log.Logger;
+    }
+}
